cbc20af845fd89e1df474d49dbe57a48cc342cf0
Bennett Hermanoff <bennett@hermanoff.dev>
2023-08-14 15:24:19 -0400

    tsify client, npm run dev on root

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { getPerson } from './api/person';
 import { getVcardFile } from './api/vcard';
-
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
@@ -12,4 +12,4 @@ app.get('/api/person/:id', getPerson);
 app.get('/api/vcard/:id', getVcardFile);
 
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(PORT, () => console.log('Server running'));
