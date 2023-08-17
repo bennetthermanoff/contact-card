83fe5673e0cc6065b056a7e85f8426f1e3b63286
Bennett Hermanoff <bennett@hermanoff.dev>
2023-08-16 23:58:24 -0400

    colors and ignore \

--- client/src/Components/MajorTags.tsx
+++ client/src/Components/MajorTags.tsx
@@ -4,19 +4,12 @@ export const MajorTags = ({ contact }:{contact:VcardJson})=>{
     const rawMajors = getEntry(contact, 'ORG') as string;
     const majors = rawMajors ? rawMajors.split(',') : [];
     //change colors every 5 seconds
-    setInterval(()=>{
-        const tags = document.getElementsByClassName('major-tag');
-        for (let i = 0; i < tags.length; i++){
-            const tag = tags[i] as HTMLElement;
-            tag.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`;
-        }
-    }, 4000);
-    
     return (
         <div className="major-tags">
             {majors.map((major, index)=>{
-                return <span style={{ backgroundColor: `hsl(${Math.random() * 360}, 100%, 90%)` }}
-                    key={index} className="major-tag">{major.trim()}</span>;
+                const h = index === 1 ? 47 : 7;
+                return <span style={{ backgroundColor: `hsl(${h}, 90%, 80%)` }}
+                    key={index} className="major-tag">{major.trim().split('\\')}</span>;
             })}
         </div>
     );
